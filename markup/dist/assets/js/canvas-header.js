0c9450195d590f15b5ad453efe3b8b266388712d
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2018-02-07 12:32:51 +0400

    Ethereum text

--- markup/dist/assets/js/canvas-header.js
+++ markup/dist/assets/js/canvas-header.js
@@ -33,8 +33,9 @@ jQuery(document).ready(function($){
 
 	//Text styles
 	var
-		TextStyleRobonomicsStart = { fontSize: 24, fontWeight: '300', fill: '#282B38'},
-		TextStyleRobonomics = { fill: '#F15A24'};
+		TextStyleStart = { fontSize: 24, fontWeight: '300', fill: '#282B38'},
+		TextStyleRobonomics = { fill: '#F15A24'},
+		TextStyleEthereum = { fill: '#535974'};
 
 
 
@@ -88,7 +89,7 @@ jQuery(document).ready(function($){
 
 
   	//Draw letters
-  	function replaceDot(array,num,x,y,text,style, side)
+  	function replaceDot(array,num,x,y,text,styleFrom,styleTo)
   		{
   			for (i = 0; i<array.length; i++)
 				{
@@ -98,38 +99,38 @@ jQuery(document).ready(function($){
 
 						s
 							.text(array[i].getBBox().cx+x, array[i].getBBox().cy+y, text)
-							.attr(TextStyleRobonomicsStart)
-							.animate(style, 5000, mina.elastic);
+							.attr(styleFrom)
+							.animate(styleTo, 5000, mina.elastic);
 					}
 				}
   		}
 
-  	function drawLetter(side,i,x,y,text,style,delay)
+  	function drawLetter(side,i,x,y,text,styleFrom,styleTo,delay)
   		{
   			setTimeout(function(){
   				if (side == 'left')
 		  			{
-		  				replaceDot(dotsLeft,i,x,y,text,style, side);
+		  				replaceDot(dotsLeft,i,x,y,text,styleFrom,styleTo);
 		  			}
 
 		  		else
 		  			{
-		  				replaceDot(dotsRight,i,x,y,text,style, side);
+		  				replaceDot(dotsRight,i,x,y,text,styleFrom,styleTo);
 		  			}
 
 		  	
 
 
 
-  			for (i = 0; i<dotsRight.length; i++)
-				{
-					s.text(dotsRight[i].node.getBoundingClientRect().x, dotsRight[i].node.getBoundingClientRect().y, i);
-				}
+  	// 		for (i = 0; i<dotsRight.length; i++)
+			// 	{
+			// 		s.text(dotsRight[i].node.getBoundingClientRect().x, dotsRight[i].node.getBoundingClientRect().y, i);
+			// 	}
 
-			for (i = 0; i<dotsLeft.length; i++)
-				{
-					s.text(dotsLeft[i].node.getBoundingClientRect().x, dotsLeft[i].node.getBoundingClientRect().y, i);
-				}
+			// for (i = 0; i<dotsLeft.length; i++)
+			// 	{
+			// 		s.text(dotsLeft[i].node.getBoundingClientRect().x, dotsLeft[i].node.getBoundingClientRect().y, i);
+			// 	}
 			}, delay);
   		}
 
@@ -141,18 +142,33 @@ jQuery(document).ready(function($){
   		{
   			var y = 6;
 
-  			drawLetter('left', 23, -3, y, 'r', TextStyleRobonomics, delayArray[0]);
-			drawLetter('left', 18, -7, y, 'o', TextStyleRobonomics, delayArray[1]);
-			drawLetter('left', 13, -7, y, 'b', TextStyleRobonomics, delayArray[2]);
-			drawLetter('left', 8, -7, y, 'o', TextStyleRobonomics, delayArray[3]);
-			drawLetter('left', 3, -7, y, 'n', TextStyleRobonomics, delayArray[4]);
-			drawLetter('right', 3, -7, y, 'o', TextStyleRobonomics, delayArray[5]);
-			drawLetter('right', 8, -10, y, 'm', TextStyleRobonomics, delayArray[6]);
-			drawLetter('right', 13, -3, y, 'i', TextStyleRobonomics, delayArray[7]);
-			drawLetter('right', 18, -7, y, 'c', TextStyleRobonomics, delayArray[8]);
-			drawLetter('right', 23, -7, y, 's', TextStyleRobonomics, delayArray[9]);
+  			drawLetter('left', 23, -3, y, 'r', TextStyleStart, TextStyleRobonomics, delayArray[0]);
+			drawLetter('left', 18, -7, y, 'o', TextStyleStart, TextStyleRobonomics, delayArray[1]);
+			drawLetter('left', 13, -7, y, 'b', TextStyleStart, TextStyleRobonomics, delayArray[2]);
+			drawLetter('left', 8, -7, y, 'o', TextStyleStart, TextStyleRobonomics, delayArray[3]);
+			drawLetter('left', 3, -7, y, 'n', TextStyleStart, TextStyleRobonomics, delayArray[4]);
+			drawLetter('right', 3, -7, y, 'o', TextStyleStart, TextStyleRobonomics, delayArray[5]);
+			drawLetter('right', 8, -10, y, 'm', TextStyleStart, TextStyleRobonomics, delayArray[6]);
+			drawLetter('right', 13, -3, y, 'i', TextStyleStart, TextStyleRobonomics, delayArray[7]);
+			drawLetter('right', 18, -7, y, 'c', TextStyleStart, TextStyleRobonomics, delayArray[8]);
+			drawLetter('right', 23, -7, y, 's', TextStyleStart, TextStyleRobonomics, delayArray[9]);
   		}
 
+
+  	// Show text 'Ethethereum'
+  	function TextEthereum(delayArray)
+		{
+			var y = 6;
+
+			drawLetter('left', 16, -3, y, 'e', TextStyleStart, TextStyleEthereum, delayArray[0]);
+			drawLetter('left', 11, -4, y, 't', TextStyleStart, TextStyleEthereum, delayArray[1]);
+			drawLetter('left', 6, -7, y, 'h', TextStyleStart, TextStyleEthereum, delayArray[2]);
+			drawLetter('left', 1, -7, y, 'e', TextStyleStart, TextStyleEthereum, delayArray[3]);
+			drawLetter('right', 1, -4, y, 'r', TextStyleStart, TextStyleEthereum, delayArray[4]);
+			drawLetter('right', 6, -7, y, 'e', TextStyleStart, TextStyleEthereum, delayArray[5]);
+			drawLetter('right', 11, -7, y, 'u', TextStyleStart, TextStyleEthereum, delayArray[6]);
+			drawLetter('right', 16, -10, y, 'm', TextStyleStart, TextStyleEthereum, delayArray[7]);
+		}
 
 
 
@@ -161,12 +177,17 @@ jQuery(document).ready(function($){
 
   	//ACTION!
 
+	// Draw dots in grid
 	setTimeout(function(){
 		drawGrid('true');
   	}, 1500);
 
-	var TextRobonomicsDelay = [2700, 2500, 2600, 2000, 2800, 3300, 3000, 3100, 3200, 2900];
-	TextRobonomics(TextRobonomicsDelay);
+
+	// Show text
+	var TextDelay = [2700, 2500, 2600, 2000, 2800, 3300, 3000, 3100, 3200, 2900];
+	TextRobonomics(TextDelay);
+	TextEthereum(TextDelay);
+
 
 
 
@@ -176,8 +197,9 @@ jQuery(document).ready(function($){
 
 		drawGrid('false');
 
-		TextRobonomicsDelay = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-		TextRobonomics(TextRobonomicsDelay);
+		TextDelay = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+		TextRobonomics(TextDelay);
+		TextEthereum(TextDelay);
 	});
 
 
