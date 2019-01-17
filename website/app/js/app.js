57cd32ed31145dd6774aa9732535cf08316b0bd2
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2019-01-17 23:52:56 +0400

    tge page (auction block)

--- website/app/js/app.js
+++ website/app/js/app.js
@@ -6,4 +6,30 @@ window.addEventListener('load', function(){
 	//show info by click, '0-show.js' required
 	show('data-show');
 
+
+	//progress for tge
+	var eTgePtogress = document.querySelector('.tge-progress-progress');
+
+	function setProgress(){
+
+		if (window.innerWidth < 1000){
+			eTgePtogress.style.width = eTgePtogress.getAttribute('data-progress');
+			eTgePtogress.style.height = '2px';
+		}
+		else{
+			eTgePtogress.style.height = eTgePtogress.getAttribute('data-progress');
+			eTgePtogress.style.width = '2px';
+		}
+	}
+
+	if ( eTgePtogress ){
+		setProgress();
+
+		window.addEventListener('resize', function(event) {
+		  setProgress();
+		});
+
+	}
+	
+
 }, false);
