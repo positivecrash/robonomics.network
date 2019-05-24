0d99085d5f141effd8213c7f8292b9e14e649883
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2019-05-24 11:09:29 +0400

    lighthouse connect

--- dapp-pages/app/js/app.js
+++ dapp-pages/app/js/app.js
@@ -1,12 +1,12 @@
-window.addEventListener('load', function(){
+// window.addEventListener('load', function(){
 
-	// //sort tags, '0-sort.js' required
-	// sort('sort', 'sort-sec', 'active');
+// 	// //sort tags, '0-sort.js' required
+// 	// sort('sort', 'sort-sec', 'active');
 
-	if( document.querySelector('.disabled')){
-		document.querySelector('.disabled').addEventListener("click", function(event){
-		  event.preventDefault()
-		});
-	}
+// 	if( document.querySelector('.disabled')){
+// 		document.querySelector('.disabled').addEventListener("click", function(event){
+// 		  event.preventDefault()
+// 		});
+// 	}
 
-}, false);
+// }, false);
--- dapp-pages/dist/assets/js/robonomics_plugins.js
+++ dapp-pages/dist/assets/js/robonomics_plugins.js
@@ -1,15 +1,15 @@
-window.addEventListener('load', function(){
+// window.addEventListener('load', function(){
 
-	// //sort tags, '0-sort.js' required
-	// sort('sort', 'sort-sec', 'active');
+// 	// //sort tags, '0-sort.js' required
+// 	// sort('sort', 'sort-sec', 'active');
 
-	if( document.querySelector('.disabled')){
-		document.querySelector('.disabled').addEventListener("click", function(event){
-		  event.preventDefault()
-		});
-	}
+// 	if( document.querySelector('.disabled')){
+// 		document.querySelector('.disabled').addEventListener("click", function(event){
+// 		  event.preventDefault()
+// 		});
+// 	}
 
-}, false);
+// }, false);
 //  Import support https://stackoverflow.com/questions/13673346/supporting-both-commonjs-and-amd
 (function(name, definition) {
     if (typeof module !== "undefined") { module.exports = definition(); }
