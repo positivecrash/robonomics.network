c234158932f3dee8203aeda436230cf0a373a597
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2019-05-28 10:37:24 +0400

    lighthouse + font icons + sidebar mixin

--- dapp-pages/dist/assets/js/robonomics_plugins.js
+++ dapp-pages/dist/assets/js/robonomics_plugins.js
@@ -2,20 +2,26 @@ window.addEventListener('load', function(){
 
 	
 	// func isVisible from 0-show.js
-	if( document.querySelector('.window-head-toggle')){
-		document.querySelector('.window-head-toggle').addEventListener("click", function(event){
-		  event.preventDefault();
+	if( document.querySelectorAll('.window-head-toggle')){
 
-		  var content = this.closest('.window').children[1];
-		  if(isVisible(content)){
-		  	content.style.display = 'none';
-		  	this.innerHTML = '+';
-		  }
-		  else{
-		  	content.style.display = 'block';
-		  	this.innerHTML = '–';
-		  }
-		});
+		var w = document.querySelectorAll('.window-head-toggle');
+
+		for (var i = 0; i < w.length; i++) {
+
+			w[i].addEventListener("click", function(event){
+			  event.preventDefault();
+
+			  var content = this.closest('.window').children[1];
+			  if(isVisible(content)){
+			  	content.style.display = 'none';
+			  	this.innerHTML = '+';
+			  }
+			  else{
+			  	content.style.display = 'block';
+			  	this.innerHTML = '–';
+			  }
+			});
+		}
 	}
 
 }, false);
