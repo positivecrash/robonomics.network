d1ac808fbdb20dd4485abd81bb342fb8f429ac4b
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2019-06-06 07:27:27 +0400

    dapp cookie for windows

--- dapp-pages/dist/assets/js/robonomics_plugins.js
+++ dapp-pages/dist/assets/js/robonomics_plugins.js
@@ -1,30 +1,65 @@
-window.addEventListener('load', function(){
 
-	
-	// func isVisible from 0-show.js
-	if( document.querySelectorAll('.window-head-toggle')){
 
-		var w = document.querySelectorAll('.window-head-toggle');
+function windowSlideAct(o, hide){
+	content = o.closest('.window').children[1];
+	w = o.closest('.window');
+
+	if ( hide ){
+  		content.style.display = 'none';
+  		o.innerHTML = '+';
+
+  		if(w.getAttribute('id')){
+  			date = new Date(new Date().getTime() + 3600 * 1000 * 24 * 365);
+			document.cookie = w.getAttribute('id')+'=hide; expires=' + date.toUTCString();
+		}
+  	}
+  	else{
+  		content.style.display = 'block';
+  		o.innerHTML = '–';
+
+  		if(w.getAttribute('id')){
+  			date = new Date(new Date().getTime() + 3600 * 1000 * 24 * 365);
+			document.cookie = w.getAttribute('id')+'=show; expires=' + date.toUTCString();
+		}
+  	}
+}
+
+
+function windowSlide(){
+	var content, wID, hide,
+	w = document.querySelectorAll('.window-head-toggle');
+
+	if(w){
 
 		for (var i = 0; i < w.length; i++) {
 
-			w[i].addEventListener("click", function(event){
-			  event.preventDefault();
+			wID = w[i].closest('.window').getAttribute('id');
+	  		
+	  		if(getCookie(wID)){
+	  			if(getCookie(wID) == 'hide') { hide = true; }
+	  			if(getCookie(wID) == 'show') { hide = false; }
 
-			  var content = this.closest('.window').children[1];
-			  if(isVisible(content)){
-			  	content.style.display = 'none';
-			  	this.innerHTML = '+';
-			  }
-			  else{
-			  	content.style.display = 'block';
-			  	this.innerHTML = '–';
-			  }
+	  			windowSlideAct(w[i], hide);
+	  		}
+
+			w[i].addEventListener("click", function(event){
+		  		event.preventDefault();
+
+		  		content = this.closest('.window').children[1];
+
+			  	// func isVisible from 0-show.js
+			  	if(isVisible(content)){ hide = true; }
+			  	else{ hide = false; }
+
+			  	windowSlideAct(this, hide);
+			  	
 			});
 		}
 	}
+}
 
-}, false);
+
+window.addEventListener('load', windowSlide, false);
 //  Import support https://stackoverflow.com/questions/13673346/supporting-both-commonjs-and-amd
 (function(name, definition) {
     if (typeof module !== "undefined") { module.exports = definition(); }
