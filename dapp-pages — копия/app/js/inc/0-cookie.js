de1f9c596b35486f3bdcfbcd1716c5b7e327e6a0
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2019-07-08 22:22:58 +0400

    moved dapp to the separatr repo, del some ald files

--- dapp-pages — копия/app/js/inc/0-cookie.js
+++
@@ -1,6 +0,0 @@
-function getCookie(name) {
-  var matches = document.cookie.match(new RegExp(
-    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-  ));
-  return matches ? decodeURIComponent(matches[1]) : undefined;
-}
